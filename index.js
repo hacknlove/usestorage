86b631a7bf0767bdcf7d0c137d0d94d2cb183c9d
antonio <antonio@pykiss.com>
2019-07-31 10:26:01 +0200

    publish

---
+++ index.js
@@ -0,0 +1,48 @@
+import browser from 'webextension-polyfill'
+import { useState, useEffect } from 'react'
+import isDifferent from 'isdifferent'
+
+export var stored = {
+  local: {},
+  sync: {}
+}
+
+export default function useStorage (key, first = null, area = 'sync') {
+  first = stored[area][key] || first
+
+  const [value, set] = useState(first)
+
+  stored[area][key] = value
+
+  browser.storage[area].get(key).then(sync => {
+    if (isDifferent(value, sync[key])) {
+      set(sync[key])
+    }
+  })
+
+  const storageOnChange = (object, _area) => {
+    if (area !== _area) {
+      return
+    }
+    if (!object[key]) {
+      return
+    }
+    if (isDifferent(value, object[key].newValue)) {
+      set(object[key].newValue)
+    }
+  }
+  useEffect(() => {
+    browser.storage.onChanged.addListener(storageOnChange)
+    return () => {
+      browser.storage.onChanged.removeListener(storageOnChange)
+    }
+  })
+  return value
+}
+
+export async function preStore (keys, area = 'sync') {
+  const sync = await browser.storage[area].get(keys)
+  keys.forEach(key => {
+    stored[area][key] = sync[area]
+  })
+}
